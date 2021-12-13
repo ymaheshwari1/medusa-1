bf463bf8f397a791b8b4bff028409aafcdee5efb
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-12-13 02:09:15 -0300

    feat(theme): added initial middleware config

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -2,7 +2,10 @@ module.exports = {
   integrations: {
     medusa: {
       location: '@vue-storefront/medusa-api/server',
-      configuration: {}
+      configuration: {
+        api: process.env.MEDUSA_API_URL || 'http://localhost:9000',
+        maxRetries: 10
+      }
     }
   }
 };
