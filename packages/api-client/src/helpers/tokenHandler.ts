86f9ff798a1f80eabd35251d9b1653efa23a70c4
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-01-29 16:23:17 +0530

    Added: logic to store the cookie in medusa client

--- packages/api-client/src/helpers/tokenHandler.ts
+++ packages/api-client/src/helpers/tokenHandler.ts
@@ -1,3 +1,11 @@
-export const tokenHandler = (response: any) => (
-  console.log('token', response.headers)
-);
+import { KeyManager } from "@medusajs/medusa-js";
+
+export const tokenHandler = (response: any) => {
+  const cookie = response.headers['set-cookie'][0]
+
+  if(cookie.includes('connect.sid')) {
+    const medusaToken = cookie.substring(cookie.indexOf('=') + 1, cookie.indexOf(';'))
+    
+    KeyManager.registerPublishableApiKey(medusaToken)
+  }
+};
